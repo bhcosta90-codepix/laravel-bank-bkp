f581509e01f4b8dcb2d31648672b17e66c951a51
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-24 09:43:46 -0300

    wip

--- src/Domain/Repository/TransactionRepository.php
+++ src/Domain/Repository/TransactionRepository.php
@@ -12,16 +12,7 @@ class TransactionRepository implements TransactionRepositoryInterface
 {
     public function registerDebit(Transaction $transaction): bool
     {
-        return (bool) \App\Models\Transaction::create([
-            'id' => $transaction->id(),
-            'account_id' => $transaction->accountFrom->id(),
-            'value' => $transaction->value,
-            'type' => TypeTransactionEnum::DEBIT,
-            'kind' => $transaction->pixKeyTo->kind,
-            'key' => $transaction->pixKeyTo->key,
-            'description' => $transaction->description,
-            'status' => $transaction->status,
-        ]);
+        return $this->register($transaction, TypeTransactionEnum::DEBIT);
     }
 
     public function save(Transaction $transaction): bool
@@ -32,6 +23,24 @@ class TransactionRepository implements TransactionRepositoryInterface
     public function find(string $id): ?Transaction
     {
         dd('Implement find() method.');
+    }
+
+    /**
+     * @param Transaction $transaction
+     * @return bool
+     */
+    private function register(Transaction $transaction, TypeTransactionEnum $type): bool
+    {
+        return (bool)\App\Models\Transaction::create([
+            'id' => $transaction->id(),
+            'account_id' => $transaction->accountFrom->id(),
+            'value' => $transaction->value,
+            'type' => $type,
+            'kind' => $transaction->pixKeyTo->kind,
+            'key' => $transaction->pixKeyTo->key,
+            'description' => $transaction->description,
+            'status' => $transaction->status,
+        ]);
     }
 
 }
