e7497d1e1382d5b54152df4462b8ed47d9ebfb25
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-25 17:00:40 -0300

    wip

--- app/Http/Resources/TransactionResource.php
+++ app/Http/Resources/TransactionResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\Enum\Transaction\TypeTransactionEnum;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,12 +15,17 @@ class TransactionResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $type = isset($this->type)
+            ? TypeTransactionEnum::from($this->type)
+            : null;
+
         return [
             'id' => (string) $this->id,
             'account_id' => $this->account_id ?? $this->accountFrom,
             'debit_id' => $this->debit_id ?? $this->debit ?? null,
-            'value' => $this->value,
+            'value' => $type ? ($type == TypeTransactionEnum::DEBIT ? $this->value * -1 : $this->value) : null,
             'kind' => $this->kind,
+            'type' => $type,
             'key' => $this->key,
             'description' => $this->description,
             'status' => $this->status,
