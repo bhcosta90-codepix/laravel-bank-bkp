3f713bad03b81c5a569c7fbfe2ddc628ecc4e6f5
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-25 11:58:33 -0300

    wip

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Account extends Authenticatable
@@ -19,5 +20,10 @@ class Account extends Authenticatable
     public function agency(): BelongsTo
     {
         return $this->belongsTo(Agency::class);
+    }
+
+    public function pix(): HasMany
+    {
+        return $this->hasMany(PixKey::class);
     }
 }
---
+++ tests/Feature/Console/Commands/Transaction/AllProcessCommandTest.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Console\Commands\Transaction\ConfirmationCommand;
+use App\Console\Commands\Transaction\CreateCommand;
+use App\Models\Account;
+use App\Models\Transaction;
+use CodePix\Bank\Application\UseCase\TransactionUseCase;
+use Tests\Stub\Services\RabbitMQService;
+
+use function Pest\Laravel\assertDatabaseHas;
+
+beforeEach(function(){
+    $this->account1 = Account::factory()->create([
+        'id' => '018b6333-2612-713c-8b6d-6e7574f2c727',
+        'name' => 'testing account_id'
+    ]);
+
+    $this->account2 = Account::factory()->create([
+        'id' => '9a7246c5-2840-46f2-b9a2-9b5ab68832a9',
+        'name' => 'testing pix key'
+    ]);
+    $this->account2->pix()->create([
+        'reference' => str()->uuid(),
+        'kind' => 'id',
+        'key' => 'ea5d1de6-66ff-4110-a21e-4d8e15c4859d',
+    ]);
+
+    Transaction::factory()->create([
+        'id' => '018b66cb-a1c2-73d4-af03-8656e51bf710',
+        'account_id' => $this->account1->id,
+        'value' => 10,
+        'kind' => 'id',
+        'key' => 'ea5d1de6-66ff-4110-a21e-4d8e15c4859d',
+    ]);
+});
+
+describe("Testing all commands to transaction", function () {
+    test("handle", function () {
+        $commandCreate = new CreateCommand();
+        $commandCreate->handle(new RabbitMQService("transaction:create"), app(TransactionUseCase::class));
+
+        assertDatabaseHas(Account::class, [
+            'id' => '9a7246c5-2840-46f2-b9a2-9b5ab68832a9',
+            'balance' => 10,
+        ]);
+
+        $commandConfirmation = new ConfirmationCommand();
+        $commandConfirmation->handle(new RabbitMQService("transaction:confirmation"), app(TransactionUseCase::class));
+
+        assertDatabaseHas(Account::class, [
+            'id' => '018b6333-2612-713c-8b6d-6e7574f2c727',
+            'balance' => -10,
+        ]);
+    });
+});
